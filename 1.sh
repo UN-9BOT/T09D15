56509cff4f6de4825112fe9bcc0d1cc5babb3f91
UN-9BOT <un.9bot@gmail.com>
2023-04-01 12:36:10 +0300

    how

--- 1.sh
+++ 1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git filter-branch --env-filter '
+git filter-branch --env-filter -f '
 OLD_EMAIL="yajairah@ev-e2.kzn.21-school.ru"
 CORRECT_EMAIL="un.9bot@gmail.com"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
