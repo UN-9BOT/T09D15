560d9046bf9fe585a3db9af3904edf6fe891ce4d
UN-9BOT <un.9bot@gmail.com>
2023-04-01 12:34:28 +0300

    wtf

--- 1.sh
+++ 1.sh
@@ -7,4 +7,8 @@ if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
 ' --tag-name-filter cat -- --branches --tags
